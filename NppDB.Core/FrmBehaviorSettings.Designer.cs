76422cd7bfc341b96b11b5086bbd84227391b759
vidovb <vidovb@taltech.ee>
2026-02-10 19:08:45 +0200

    updated behavior setting ui

--- NppDB.Core/FrmBehaviorSettings.Designer.cs
+++ NppDB.Core/FrmBehaviorSettings.Designer.cs
@@ -9,6 +9,12 @@ namespace NppDB.Core
         /// </summary>
         private IContainer components = null;
 
+        private System.Windows.Forms.TableLayoutPanel layoutRoot;
+        private System.Windows.Forms.GroupBox grpBehavior;
+        private System.Windows.Forms.TableLayoutPanel layoutOptions;
+        private System.Windows.Forms.CheckBox destructiveSelectIntoCheckbox;
+        private System.Windows.Forms.CheckBox newTabCheckbox;
+
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
@@ -31,15 +37,65 @@ namespace NppDB.Core
         /// </summary>
         private void InitializeComponent()
         {
+            this.layoutRoot = new System.Windows.Forms.TableLayoutPanel();
+            this.grpBehavior = new System.Windows.Forms.GroupBox();
+            this.layoutOptions = new System.Windows.Forms.TableLayoutPanel();
             this.destructiveSelectIntoCheckbox = new System.Windows.Forms.CheckBox();
             this.newTabCheckbox = new System.Windows.Forms.CheckBox();
+            this.layoutRoot.SuspendLayout();
+            this.grpBehavior.SuspendLayout();
+            this.layoutOptions.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // layoutRoot
+            // 
+            this.layoutRoot.ColumnCount = 1;
+            this.layoutRoot.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutRoot.Controls.Add(this.grpBehavior, 0, 0);
+            this.layoutRoot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutRoot.Location = new System.Drawing.Point(10, 10);
+            this.layoutRoot.Name = "layoutRoot";
+            this.layoutRoot.RowCount = 1;
+            this.layoutRoot.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutRoot.Size = new System.Drawing.Size(382, 141);
+            this.layoutRoot.TabIndex = 0;
+            // 
+            // grpBehavior
+            // 
+            this.grpBehavior.Controls.Add(this.layoutOptions);
+            this.grpBehavior.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpBehavior.Location = new System.Drawing.Point(3, 3);
+            this.grpBehavior.Name = "grpBehavior";
+            this.grpBehavior.Padding = new System.Windows.Forms.Padding(10);
+            this.grpBehavior.Size = new System.Drawing.Size(376, 135);
+            this.grpBehavior.TabIndex = 0;
+            this.grpBehavior.TabStop = false;
+            this.grpBehavior.Text = "Behavior";
+            // 
+            // layoutOptions
+            // 
+            this.layoutOptions.ColumnCount = 1;
+            this.layoutOptions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutOptions.Controls.Add(this.destructiveSelectIntoCheckbox, 0, 0);
+            this.layoutOptions.Controls.Add(this.newTabCheckbox, 0, 1);
+            this.layoutOptions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutOptions.Location = new System.Drawing.Point(10, 23);
+            this.layoutOptions.Name = "layoutOptions";
+            this.layoutOptions.RowCount = 2;
+            this.layoutOptions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize));
+            this.layoutOptions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.AutoSize));
+            this.layoutOptions.Size = new System.Drawing.Size(356, 102);
+            this.layoutOptions.TabIndex = 0;
             // 
             // destructiveSelectIntoCheckbox
             // 
-            this.destructiveSelectIntoCheckbox.Location = new System.Drawing.Point(12, 12);
+            this.destructiveSelectIntoCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                                                                                          | System.Windows.Forms.AnchorStyles.Right)));
+            this.destructiveSelectIntoCheckbox.AutoEllipsis = true;
+            this.destructiveSelectIntoCheckbox.Location = new System.Drawing.Point(3, 3);
+            this.destructiveSelectIntoCheckbox.MinimumSize = new System.Drawing.Size(0, 28);
             this.destructiveSelectIntoCheckbox.Name = "destructiveSelectIntoCheckbox";
-            this.destructiveSelectIntoCheckbox.Size = new System.Drawing.Size(248, 47);
+            this.destructiveSelectIntoCheckbox.Size = new System.Drawing.Size(350, 34);
             this.destructiveSelectIntoCheckbox.TabIndex = 0;
             this.destructiveSelectIntoCheckbox.Text = "Enable destructive SELECT INTO (MSAccess)";
             this.destructiveSelectIntoCheckbox.UseVisualStyleBackColor = true;
@@ -47,9 +103,13 @@ namespace NppDB.Core
             // 
             // newTabCheckbox
             // 
-            this.newTabCheckbox.Location = new System.Drawing.Point(12, 65);
+            this.newTabCheckbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                                                                              | System.Windows.Forms.AnchorStyles.Right)));
+            this.newTabCheckbox.AutoEllipsis = true;
+            this.newTabCheckbox.Location = new System.Drawing.Point(3, 43);
+            this.newTabCheckbox.MinimumSize = new System.Drawing.Size(0, 28);
             this.newTabCheckbox.Name = "newTabCheckbox";
-            this.newTabCheckbox.Size = new System.Drawing.Size(248, 47);
+            this.newTabCheckbox.Size = new System.Drawing.Size(350, 34);
             this.newTabCheckbox.TabIndex = 1;
             this.newTabCheckbox.Text = "Create new tab every time for queries";
             this.newTabCheckbox.UseVisualStyleBackColor = true;
@@ -59,18 +119,24 @@ namespace NppDB.Core
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 129);
-            this.Controls.Add(this.newTabCheckbox);
-            this.Controls.Add(this.destructiveSelectIntoCheckbox);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(402, 161);
+            this.Controls.Add(this.layoutRoot);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(320, 190);
             this.Name = "FrmBehaviorSettings";
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Behavior Settings";
+            this.layoutRoot.ResumeLayout(false);
+            this.grpBehavior.ResumeLayout(false);
+            this.layoutOptions.ResumeLayout(false);
             this.ResumeLayout(false);
         }
-
-        private System.Windows.Forms.CheckBox destructiveSelectIntoCheckbox;
-
-        private System.Windows.Forms.CheckBox newTabCheckbox;
 
         #endregion
     }
