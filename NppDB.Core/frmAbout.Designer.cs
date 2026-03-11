416abdd50c3bab49834157d3266c2461e301afc6
vidovb <vidovb@taltech.ee>
2026-03-11 23:04:47 +0200

    updated about info

--- NppDB.Core/frmAbout.Designer.cs
+++ NppDB.Core/frmAbout.Designer.cs
@@ -33,56 +33,60 @@ namespace NppDB.Core
         {
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblVer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // linkLabel1
             // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(47, 135);
+            this.linkLabel1.Location = new System.Drawing.Point(95, 209);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(199, 12);
+            this.linkLabel1.Size = new System.Drawing.Size(182, 13);
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://github.com/gutkyu/NppDB";
+            this.linkLabel1.Text = "https://github.com/azyaoffic/NppDB";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(77, 31);
+            this.label1.Location = new System.Drawing.Point(124, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "NppDB";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(48, 109);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Copyright (c) 2014 Sangkyu Jung";
-            // 
             // lblVer
             // 
             this.lblVer.AutoSize = true;
-            this.lblVer.Location = new System.Drawing.Point(111, 84);
+            this.lblVer.Location = new System.Drawing.Point(157, 83);
             this.lblVer.Name = "lblVer";
-            this.lblVer.Size = new System.Drawing.Size(48, 12);
+            this.lblVer.Size = new System.Drawing.Size(42, 13);
             this.lblVer.TabIndex = 3;
             this.lblVer.Text = "Version";
             // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(362, 86);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Copyright (c) 2026 Viktor Dovbnia\r\n\r\nOriginal Copyright (c) 2014 Sangkyu Jung\r\n\r\n" + "With additions from: Priit Post, Andres Eelma, Sten Ärm";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // frmAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 192);
-            this.Controls.Add(this.lblVer);
+            this.ClientSize = new System.Drawing.Size(386, 245);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVer);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -95,15 +99,14 @@ namespace NppDB.Core
             this.Load += new System.EventHandler(this.frmAbout_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
-        private LinkLabel linkLabel1;
-        private Label label1;
-        private Label label2;
-        private Label lblVer;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVer;
 
     }
 }
